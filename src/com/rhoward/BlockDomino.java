75e2bc2fdd9b237caa749b89657b112d44e6c5a6
a-out <richbone227@gmail.com>
2013-02-05 12:13:20 -0500

    mid-refactor

--- src/com/rhoward/BlockDomino.java
+++ src/com/rhoward/BlockDomino.java
@@ -2,36 +2,14 @@ package com.rhoward;
 
 import static com.rhoward.Block.BlockType;
 
-public class BlockDomino extends Domino{
+public class BlockDomino extends Domino {
 
-    public BlockDomino(BlockType type, float x, float y) {
-        super(type, 4, x, y);
+    public BlockDomino(BlockType type) {
+        super(type);
     }
 
     @Override
-    protected void calculateBlockPositions() {
-        // top-left block
-        blocks[0].setX(this.x);
-        blocks[0].setY(this.y);
-
-        // top-right block
-        blocks[1].setX(this.x + BLOCK_WIDTH + 1);
-        blocks[1].setY(this.y);
-
-        // bottom-right block
-        blocks[2].setX(this.x + BLOCK_WIDTH + 1);
-        blocks[2].setY(this.y + BLOCK_HEIGHT + 1);
-
-        // bottom-left block
-        blocks[3].setX(this.x);
-        blocks[3].setY(this.y + BLOCK_HEIGHT + 1);
-    }
-
-    public float getHeight() {
-        return BLOCK_HEIGHT * 2;
-    }
-
-    public float getWidth() {
-        return BLOCK_WIDTH * 2;
+    public int[] getGrid() {
+        return new int[];
     }
 }
--- src/com/rhoward/Domino.java
+++ src/com/rhoward/Domino.java
@@ -4,51 +4,14 @@ import static com.rhoward.Block.BlockType;
 
 public abstract class Domino {
 
-    protected Block[] blocks;
-    protected BlockType type;
-    protected int numBlocks;
-    protected float x, y;
-    protected float velocityX, velocityY;
-
     protected final float BLOCK_WIDTH = Block.getBLOCK_WIDTH();
     protected final float BLOCK_HEIGHT = Block.getBLOCK_HEIGHT();
 
-    public Domino(BlockType type, int numBlocks, float x, float y) {
+    BlockType type;
+
+    public Domino(BlockType type) {
         this.type = type;
-        this.numBlocks = numBlocks;
-        this.x = x;
-        this.y = y;
-        this.velocityX = 0;
-        this.velocityY = 0;
-
-        blocks = new Block[this.numBlocks];
-
-        for (int i = 0; i < this.numBlocks; i++)
-            this.blocks[i] = new Block(this.type, 0, 0);
-
-        calculateBlockPositions();
     }
 
-    public void update(int delta) {
-        this.x += ((float) delta / 100) * this.velocityX;
-        this.y += ((float) delta / 100) * this.velocityY;
-
-        calculateBlockPositions();
-    }
-
-    public void draw() {
-        for (int i = 0; i < numBlocks; i++) {
-            blocks[i].draw();
-        }
-    }
-
-    protected abstract void calculateBlockPositions();
-
-    public void modVelocityX(float velocityX) {
-        this.velocityX += velocityX;
-    }
-
-    public void modVelocityY(float velocityY) {
-        this.velocityY += velocityY;
-    }
+    public abstract int[] getGrid();
 }
