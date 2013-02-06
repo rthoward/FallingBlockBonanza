c173e75b927827622bcbbe3d0f5e9be7c94d774d
a-out <richbone227@gmail.com>
2013-02-06 12:05:56 -0500

    for randomly generating new dominos

---
+++ src/com/rhoward/DominoFactory.java
@@ -0,0 +1,60 @@
+package com.rhoward;
+
+import java.util.Random;
+
+public class DominoFactory {
+
+    private final int NUM_PIECES = 3;
+    private final int NUM_COLORS = 4;
+
+    private int startx, starty;
+    private Random rand;
+
+    public DominoFactory(int startx, int starty) {
+        this.rand = new Random();
+        this.startx = startx;
+        this.starty = starty;
+    }
+
+    public Domino newDomino() {
+        Domino newDomino;
+        int newTypeRand = rand.nextInt(NUM_PIECES) + 1;
+        int newColorRand = rand.nextInt(NUM_COLORS) + 1;
+        Block.BlockType newType;
+
+        switch (newColorRand) {
+            case 1:
+                newType = Block.BlockType.RED;
+                break;
+            case 2:
+                newType = Block.BlockType.BLUE;
+                break;
+            case 3:
+                newType = Block.BlockType.GREEN;
+                break;
+            case 4:
+                newType = Block.BlockType.YELLOW;
+                break;
+            default:
+                newType = Block.BlockType.EMPTY;
+                break;
+        }
+
+        switch (newTypeRand) {
+            case 1:
+                newDomino = new BlockDomino(newType, startx, starty);
+                break;
+            case 2:
+                newDomino = new LDomino(newType, startx, starty);
+                break;
+            case 3:
+                newDomino = new ZDomino(newType, startx, starty);
+                break;
+            default:
+                newDomino = null;
+                break;
+        }
+
+        return newDomino;
+    }
+}
