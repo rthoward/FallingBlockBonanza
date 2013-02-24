c3934b3e1c064fa495a78e37aee18371a6dcdd65
a-out <richbone227@gmail.com>
2013-02-23 22:54:54 -0500

    added new S domino

---
+++ src/com/rhoward/SDomino.java
@@ -0,0 +1,30 @@
+package com.rhoward;
+
+import com.sun.xml.internal.ws.api.server.SDDocument;
+
+public class SDomino extends Domino {
+
+    public SDomino(Block.BlockType type, int x, int y) {
+        super(type, x, y);
+
+        this.normalShape = "000,011,110";
+        this.leftShape = "010,011,001";
+        this.downShape = "011,110,000";
+        this.rightShape = "";
+    }
+
+    @Override
+    public Domino rotate() {
+        SDomino newState = new SDomino(this.type, this.center.getX(), this.center.getY());
+        newState.rotateState = this.nextRotation();
+
+        return  newState;
+    }
+
+    @Override
+    public Domino translate(int x, int y) {
+        SDomino translated = new SDomino(this.type, this.getX() + x, this.getY() + y);
+        translated.rotateState = this.rotateState;
+        return translated;
+    }
+}
