e6574aef8b177dd4f121e4b6fcb526d718c455b6
a-out <richbone227@gmail.com>
2013-02-28 15:39:21 -0500

    created a start menu with its own input handler

---
+++ src/com/rhoward/menu/Menu.java
@@ -0,0 +1,86 @@
+package com.rhoward.menu;
+
+import static org.lwjgl.opengl.GL11.*;
+
+
+public class Menu {
+
+    enum MainMenuSelection {
+        RESUME, RESET, EXIT
+    }
+
+    private MainMenuSelection selection = MainMenuSelection.RESUME;
+    StringDrawer stringDrawer = new StringDrawer(8);
+    private MenuInputHandler input;
+    private boolean isActive = false;
+    private Color defaultColor = new Color(255.0f, 255.0f, 152.0f);
+    private Color selectedColor = new Color(100.0f, 0.0f, 0.0f);
+
+    public Menu() {
+        input = new MenuInputHandler(this);
+    }
+
+    public void logic() {
+        this.input.processInput();
+        System.out.println("selected: " + this.selection.toString());
+    }
+
+    public void draw() {
+
+        if (!isActive)
+            return;
+
+        int posX = 200;
+        int posY = 100;
+
+        glColor3f(0.0f, 225.0f, 225.0f);
+        glBegin(GL_QUADS);
+            glVertex2f(0,0);
+            glVertex2f(640, 0);
+            glVertex2f(640, 480);
+            glVertex2f(0, 480);
+        glEnd();
+
+        Color resumeColor = this.defaultColor;
+        Color resetColor = this.defaultColor;
+        Color exitColor = this.defaultColor;
+
+        if (this.selection == MainMenuSelection.RESUME)
+            resumeColor = this.selectedColor;
+        else if (this.selection == MainMenuSelection.RESET)
+            resetColor = this.selectedColor;
+        else
+            exitColor = this.selectedColor;
+
+        this.stringDrawer.drawLine("RESUME", posX, posY, resumeColor);
+        this.stringDrawer.drawLine("RESET ", posX, posY + 80, resetColor);
+        this.stringDrawer.drawLine("EXIT  ", posX, posY + 160, exitColor);
+    }
+
+    public void moveDown() {
+        if (this.selection == MainMenuSelection.RESUME)
+            this.selection = MainMenuSelection.RESET;
+        else if (this.selection == MainMenuSelection.RESET)
+            this.selection = MainMenuSelection.EXIT;
+        else
+            this.selection = MainMenuSelection.RESUME;
+    }
+
+    public void moveUp() {
+        if (this.selection == MainMenuSelection.RESUME)
+            this.selection = MainMenuSelection.EXIT;
+        else if (this.selection == MainMenuSelection.RESET)
+            this.selection = MainMenuSelection.RESUME;
+        else
+            this.selection = MainMenuSelection.RESET;
+    }
+
+
+    public void activate() {
+        this.isActive = true;
+    }
+
+    public void deactivate() {
+        this.isActive = false;
+    }
+}
---
+++ src/com/rhoward/menu/MenuInputHandler.java
@@ -0,0 +1,36 @@
+package com.rhoward.menu;
+
+import org.lwjgl.input.Keyboard;
+
+public class MenuInputHandler {
+
+    private Menu menu;
+
+    public MenuInputHandler(Menu menu) {
+        this.menu = menu;
+    }
+
+    public void processInput() {
+
+        while (Keyboard.next()) {
+            switch (Keyboard.getEventKey()) {
+                case Keyboard.KEY_UP:
+                    if (Keyboard.getEventKeyState())
+                        this.menu.moveUp();
+                    break;
+                case Keyboard.KEY_DOWN:
+                    if (Keyboard.getEventKeyState())
+                        this.menu.moveDown();
+                    break;
+                case Keyboard.KEY_ESCAPE :
+                    if (Keyboard.getEventKeyState())
+                        this.menu.deactivate();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+}
