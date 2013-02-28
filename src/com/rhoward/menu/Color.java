ccd921745c8f1fa45eae37c93ac2daef4773e4d0
a-out <richbone227@gmail.com>
2013-02-28 15:39:35 -0500

    created a color class for easier parameter passing into StringDrawer

---
+++ src/com/rhoward/menu/Color.java
@@ -0,0 +1,38 @@
+package com.rhoward.menu;
+
+
+public class Color {
+
+    private float red, green, blue;
+
+    public Color(float red, float green, float blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public float getBlue() {
+        return blue;
+    }
+
+    public void setBlue(float blue) {
+        this.blue = blue;
+    }
+
+    public float getRed() {
+        return red;
+    }
+
+    public void setRed(float red) {
+        this.red = red;
+    }
+
+    public float getGreen() {
+        return green;
+    }
+
+    public void setGreen(float green) {
+        this.green = green;
+    }
+
+}
